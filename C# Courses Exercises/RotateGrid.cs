0feb62f15028f2a41717e54b0f5f52920091fd2d
Rebecca-kve <55534811+Rebecca-kve@users.noreply.github.com>
2022-12-20 14:44:20 +0100

    Challenge: rotate grid

--- C# Courses Exercises/RotateGrid.cs
+++ C# Courses Exercises/RotateGrid.cs
@@ -8,9 +8,11 @@ PrintArray(Grid);
 Console.WriteLine();
 int[,] RotatedGrid = new int[4,4];
 
+RotatedGrid = RotateGridRight(Grid);
+PrintArray(RotatedGrid);
+Console.WriteLine();
 RotatedGrid = RotateGridLeft(Grid);
 PrintArray(RotatedGrid);
-
 static int[,] RotateGridLeft(int[,] grid)
 {
     int[,] result = new int[grid.GetLength(0), grid.GetLength(1)];
@@ -19,8 +21,22 @@ static int[,] RotateGridLeft(int[,] grid)
     {
         for (int col = 0; col < grid.GetLength(1); col++)
         {
-            result[col, grid.GetLength(0) - row - 1] = grid[row, col]; ;
+            result[grid.GetLength(1)-col-1, row] = grid[row, col];
         }  
+    }
+    return result;
+}
+
+static int[,] RotateGridRight(int[,] grid)
+{
+    int[,] result = new int[grid.GetLength(0), grid.GetLength(1)];
+
+    for (int row = 0; row < grid.GetLength(0); row++)
+    {
+        for (int col = 0; col < grid.GetLength(1); col++)
+        {
+            result[col, grid.GetLength(0) - row - 1] = grid[row, col];
+        }
     }
     return result;
 }
@@ -30,8 +46,8 @@ static void PrintArray(int[,] grid)
     {
         for (int col = 0; col < grid.GetLength(1); col++)
         {
-            Console.Write("{0} ", grid[col, row]);
+            Console.Write("{0} ", grid[row, col]);
         }
         Console.WriteLine();
     }
-}  
+}   
